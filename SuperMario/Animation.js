1acfe90ac41d5c86f0cb0fa8ac5a194f9723a3b6
sealiuhailin <1499252561@qq.com>
2017-08-20 23:59:27 +0800

    Update Animation.js

--- SuperMario/Animation.js
+++ SuperMario/Animation.js
@@ -1,92 +1,33 @@
 
-// //Animation类
-// //cfg为Object类型的参数集，其属性会覆盖Animation原型中定义的同名属性
-// function Animation (cfg) {
-//     for (var attr in cfg) {   //in只要能访问到的对象不管在实例中还是在原型中都返回true
-//     	this[attr] = cfg[attr];
-//     }
-// }
-// Animation.prototype = {
-// 	constructor: Animation,
-// 	//Animation包含的Frame,类型： 数组
-// 	frames: null,
-// 	//包含的Frame数目
-// 	frameCount: -1,
-// 	//所使用的图片id（在ImgCache中存放的Key），字符串类型
-// 	img: null,     //"player"
-// 	currentFrame: null,
-// 	currentFrameIndex: -1,
-// 	currentFramePlayed: -1,
-// 	//初始化Animation
-// 	init: function () {
-// 		//根据id取得Image对象
-// 		this.img = imgCache[this.img] || this.img;
-
-// 		this.frames = this.frames || [];
-// 		// this.frameCount = this.frames || [];
-// 		this.frameCount = this.frames.length;
-
-// 		//缺省从第0帧开始播放
-// 		this.currentFrameIndex = 0;
-// 		this.currentFrame = this.frames[this.currentFrameIndex];
-// 		this.currentFramePlayed = 0;
-// 	},
-// 	//更新Animation状态。deltaTime表示时间的变化量
-// 	update: function (deltaTime) {
-// 		//判断当前Frame是否已经播放完成
-// 		if (this.currentFramePlayed >= this.currentFrame.duration) {
-// 			//播放下一帧
-// 			if (this.currentFrameIndex >= this.frameCount - 1) {
-// 				//当前是最后一帧，则播放第0帧
-// 				this.currentFrameIndex = 0;
-// 			} else {
-// 				//播放下一帧
-// 				this.currentFrameIndex++;
-// 			}
-// 			//设置当前帧信息
-// 			this.currentFrame = this.frames[this.currentFrameIndex];
-// 			this.currentFramePlayed = 0;
-// 		} else {
-// 			//增加当前帧的已播放时间
-// 		    this.currentFramePlayed += deltaTime;
-// 		}
-// 	},
-// 	//绘制Animation
-// 	draw: function (gc, x, y) {
-// 		var f = this.currentFrame;
-// 		gc.drawImage(this.img, f.x, f.y, f.w, f.h, x, y, f.w, f.h);
-// 	}
-// }
-
-
-// Animation类.
-// cfg为Object类型的参数集, 其属性会覆盖Animation原型中定义的同名属性.
-function Animation(cfg){
-	for (var attr in cfg ){
-		this[attr]=cfg[attr];
+//JS逐帧动画
+// Animation构造函数，
+// cfg参数为一个对象，包括图片的名称img和动画的帧frames
+function Animation (cfg) {
+	//枚举对象的属性
+	for (var attr in cfg ) {
+		this[attr] = cfg[attr];
 	}
 }
 
-
-Animation.prototype={
-	constructor :Animation ,
+Animation.prototype = {
+	constructor: Animation,
 
 	// Animation 包含的Frame, 类型:数组
-	frames : null,
+	frames: null,
 	// 包含的Frame数目
-	frameCount : -1 ,
+	frameCount: -1,
 	// 所使用的图片id(在ImgCache中存放的Key), 字符串类型. 
-	img : null,
-	currentFrame : null ,
-	currentFrameIndex : -1 ,
-	currentFramePlayed : -1 ,
+	img: null,
+	currentFrame: null,
+	currentFrameIndex: -1,
+	currentFramePlayed: -1,
 	
 	// 初始化Animation
-	init : function(){
+	init: function () {
 		// 根据id取得Image对象
-		this.img = imgCache[this.img]||this.img;
+		this.img = ImgCache[this.img] || this.img;
 		
-		this.frames=this.frames||[];
+		this.frames=this.frames || [];
 		this.frameCount = this.frames.length;
 		
 		// 缺省从第0帧播放
@@ -94,59 +35,37 @@ Animation.prototype={
 	},
 	
 	//设置当前帧
-	setFrame : function(index){
-		this.currentFrameIndex=index;
-		this.currentFrame=this.frames[index];
-		this.currentFramePlayed=0;	
+	setFrame: function (index) {
+		this.currentFrameIndex = index;
+		this.currentFrame = this.frames[index];
+		this.currentFramePlayed = 0;	
 	},
 	
 	// 更新Animation状态. deltaTime表示时间的变化量.
-	update : function(deltaTime){
+	update: function (deltaTime) {
 		//判断当前Frame是否已经播放完成, 
-		if (this.currentFramePlayed>=this.currentFrame.duration){
+		if (this.currentFramePlayed >= this.currentFrame.duration) {
 			//播放下一帧
 			
-			if (this.currentFrameIndex >= this.frameCount-1){
+			if (this.currentFrameIndex >= this.frameCount - 1) {
 				//当前是最后一帧,则播放第0帧
-				this.currentFrameIndex=0;
-			}else{
-				//播放下一帧
+				this.currentFrameIndex = 0;
+			} else {
+				//按顺序播放下一帧
 				this.currentFrameIndex++;
 			}
 			//设置当前帧信息
 			this.setFrame(this.currentFrameIndex);
 		
-		}else{
-			//增加当前帧的已播放时间.
+		} else {
+			//增加当前帧的已播放时间
 			this.currentFramePlayed += deltaTime;
 		}
 	},
 	
 	//绘制Animation
-	draw : function(gc,x,y){
-		var f=this.currentFrame;
+	draw: function (gc, x, y) {
+		var f = this.currentFrame;
 		gc.drawImage(this.img, f.x , f.y, f.w, f.h , x, y, f.w, f.h );
 	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
